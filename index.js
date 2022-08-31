950cf0e1985c0820eb9424ae90f18bba9099affe
rowena fonseka <rafonseka@gmail.com>
2022-08-30 23:05:14 -0400

    Install cors package, added middlewares to index.js. Added static images to public folder

--- index.js
+++ index.js
@@ -2,7 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
-const axios = require("axios");
+const cors = require("cors");
+const videoRoutes = require("./routes/videos");
+
+// middlewares
+
+// cors to allow front-end to interact with API
+app.use(cors());
+
+// middleware to post JSON in request body
+app.use(express.json());
+
+// use static image files
+app.use(express.static("./public"));
+
+// Routes
+app.use("/videos", videoRoutes);
 
 app.listen(PORT, () => {
   console.log("i hope this works" + PORT);
