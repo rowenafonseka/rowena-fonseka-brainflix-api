4b662480c9fc5d72517cc50e8a99c1ac60ab9a4f
rowena fonseka <rafonseka@gmail.com>
2022-08-31 13:54:54 -0400

    Just testing if things work and had to comment out an undefined file

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
-const cors = require("cors");
-const videoRoutes = require("./routes/videos");
+// const cors = require("cors");
+// const videoRoutes = require("./routes/videos");
 
 // middlewares
 
 // cors to allow front-end to interact with API
-app.use(cors());
+// app.use(cors());
 
-// middleware to post JSON in request body
-app.use(express.json());
+// // middleware to post JSON in request body
+// app.use(express.json());
 
-// use static image files
-app.use(express.static("./public"));
+// // use static image files
+// app.use(express.static("./public"));
 
 // Routes
-app.use("/videos", videoRoutes);
+// app.use("/videos", videoRoutes);
 
 app.listen(PORT, () => {
   console.log("i hope this works" + PORT);
