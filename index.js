47bf3ed032e1a8a52c81807be62ac0f67a603ea1
rowena fonseka <rafonseka@gmail.com>
2022-09-01 22:48:21 -0400

    Worked on post endpoit for video routes. Still need to fix bugs

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const videoRoutes = require("./routes/videos");
 // middlewares
 
 // cors to allow front-end to interact with API
-// app.use(cors());
+app.use(cors());
 
 // // middleware to post JSON in request body
-// app.use(express.json());
+app.use(express.json());
 
 // // use static image files
-// app.use(express.static("./public"));
+app.use(express.static("./public"));
 
 // Routes
-// app.use("/videos", videoRoutes);
+app.use("/videos", videoRoutes);
 
 app.listen(PORT, () => {
-  console.log("i hope this works" + PORT);
+  console.log("i hope this works");
 });
--- routes/videos.js
+++ routes/videos.js
@@ -28,7 +28,40 @@ router.get("/", (req, res) => {
 router.get("/:videoId", (req, res) => {
   const selectedVideo = videos.find((video) => video.id === req.params.videoId);
 
-  res.json(selectedVideo);
+  if (!selectedVideo) {
+    return res.status(404).send("Video not found");
+  }
 
-  //   add something for if video doesn't exist
+  res.json(selectedVideo);
 });
+
+// third endpoint to post video
+router.post("/", (req, res) => {
+  // grab function for parsing json video above
+  const updateVideoList = videoDetails();
+
+  // create a new object for new video
+  const newVideo = {
+    // id:
+    // title: req.body.title,
+    channel: "Row Your Boat",
+    image: "http://localhost:8080/images/Upload-video-preview.jpg",
+    description: req.body.description,
+    views: "0",
+    likes: "0",
+    duration: "5:00",
+    video: "https://project-2-api.herokuapp.com/stream",
+    // timestamp: Date.now(),
+    comments: [],
+  };
+
+  // push new video to updated video list
+  updateVideoList.push(newVideo);
+
+  // persist data to videos.json
+  fs.writeFileSync("./data/videos.json", JSON.stringify(updateVideoList));
+
+  res.status(200).json(newVideo);
+});
+
+module.exports = router;
